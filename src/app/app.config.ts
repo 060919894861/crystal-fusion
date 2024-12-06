37ebd5eef535795c0a2c7a4e5d1127ea9ea8f5d5
Michell Alejandra Giraldo Arcila <michellgiraldo98@gmail.com>
2024-12-05 21:45:26 -0500

    Inicio y registro incompletos

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,8 +2,31 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import {
+  provideClientHydration,
+  withEventReplay,
+} from '@angular/platform-browser';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'crystalfusion-a0304',
+        appId: '1:40617535779:web:dade03c7d2a1434e224496',
+        storageBucket: 'crystalfusion-a0304.firebasestorage.app',
+        apiKey: 'AIzaSyC3eCB79hE2Ax1rViYAkHqGSX1zqOFedZg',
+        authDomain: 'crystalfusion-a0304.firebaseapp.com',
+        messagingSenderId: '40617535779',
+        measurementId: 'G-WXFEGCX0JW',
+      })
+    ),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+  ],
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'Auth',
+    loadChildren: () => import('./Auth/features/auth.routes'),
+  }
+];
